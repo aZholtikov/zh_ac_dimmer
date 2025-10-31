e4c4ed66638cbf92d18bf74bfc0777dca10d0786
Alexey Zholtikov <git@zh.com.ru>
2025-10-31 10:30:11 +0300

    doc: update some comments for doxygen style

--- include/zh_ac_dimmer.h
+++ include/zh_ac_dimmer.h
@@ -1,3 +1,7 @@
+/**
+ * @file zh_ac_dimmer.h
+ */
+
 #pragma once
 
 #include "freertos/FreeRTOS.h"
@@ -7,6 +11,9 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 
+/**
+ * @brief AC dimmer initial default values.
+ */
 #define ZH_AC_DIMMER_INIT_CONFIG_DEFAULT() \
     {                                      \
         .zero_cross_gpio = GPIO_NUM_MAX,   \
@@ -17,11 +24,13 @@
 extern "C"
 {
 #endif
-
-    typedef struct // Structure for initial initialization of AC dimmer.
+    /**
+     * @brief Structure for initial initialization of AC dimmer.
+     */
+    typedef struct
     {
-        uint8_t zero_cross_gpio; // Zero cross GPIO.
-        uint8_t triac_gpio;      // Triac GPIO.
+        uint8_t zero_cross_gpio; /*!< Zero cross GPIO. */
+        uint8_t triac_gpio;      /*!< Triac GPIO. */
     } zh_ac_dimmer_init_config_t;
 
     /**
